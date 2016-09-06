ec60a57437d537147494563824b283e95c711085
Emmanuel Acevedo <Emmanuel Acevedo>
2016-09-06 02:41:25 -0500

    fulldashboard

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+
+//----------------------------modal script------------------------------------------
 // Get the modal
 var modal = document.getElementById('id01');
 
@@ -9,3 +11,40 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
+
+//---------------------------slider--------------------------------------
+
+var slideIndex = 1;
+
+function plusDivs(n) {
+slideIndex = slideIndex + n;
+showDivs(slideIndex);
+}
+
+function showDivs(n) {
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
+
+window.setInterval(plusDivs(+1), 2000);
+
+
+//-------------------Accordions------------------------------------
+function myAccFunc(id) {
+    var x = document.getElementById(id);
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+        x.previousElementSibling.className += " w3-dark-grey";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+        x.previousElementSibling.className =
+        x.previousElementSibling.className.replace(" w3-dark-grey", "");
+    }
+}
+
